7586757c6e5baf6214a0294b6afea6c9dfe7c353
jbehringer95 <justin_caise95@gmail.com>
2020-09-24 19:21:43 -0500

    Update
    put the ability to replay the game

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ from phrase import Phrase
 
 
 if __name__ =='__main__':
-    game = Game()
-    game.start()
+    
+    
+    while True:
+        game =Game()
+        game.start()
+        user_input = input('Would you like to play another game. y/n \n').lower()
+        if user_input == 'n':
+            break
