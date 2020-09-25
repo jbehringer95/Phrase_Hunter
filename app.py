f3af69ff7e91103299f29227cd0adeb97b251404
jbehringer95 <justin_caise95@gmail.com>
2020-09-24 19:49:53 -0500

    update

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ from phrase import Phrase
 
 
 if __name__ =='__main__':
-    
+    game =Game()
+    game.start()
     
     while True:
-        game =Game()
-        game.start()
         user_input = input('Would you like to play another game. y/n \n').lower()
-        if user_input == 'n':
+        if user_input != 'n' and user_input != 'y':
+            print('That is not an invalid option please put a y or n')
+        
+        elif user_input == 'y':
+            game = Game()
+            game.start()
+
+        else:
             break
+
+        
+            
+
+        
